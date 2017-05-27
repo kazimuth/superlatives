5281018955a8da7b94d13abaf25be94bc807319e
James Gilles <jhgilles@mit.edu>
2017-05-27 05:26:37 -0400

    redirect locally

--- local.py
+++ local.py
@@ -2,4 +2,4 @@ import imp
 main = imp.load_source('app', 'index.fcgi')
 
 if __name__ == '__main__':
-	main.app.run(host='0.0.0.0', port=5000)
+    main.app.run(host='0.0.0.0', port=5000, ssl_context=('secrets/local.crt', 'secrets/local.key'))
